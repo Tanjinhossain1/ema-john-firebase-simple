575772f6180c0ff612e5bd5d7a1f5d4c954a920a
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-10 13:53:50 +0600

    requaredauth added done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './Login.css';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useAuthState, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const [
         signInWithEmailAndPassword,
         user,
         loading,
         error,
-      ] = useSignInWithEmailAndPassword(auth);
-    
-
+    ] = useSignInWithEmailAndPassword(auth);
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/shop";
 
     const handleEmail = (event) => {
         setEmail(event.target.value)
@@ -23,45 +23,45 @@ const Login = () => {
     const handlePassword = (event) => {
         setPassword(event.target.value)
     }
-    if(user){
-        navigate('/shop');
+    if (user) {
+        navigate(from);
     }
-    const signInEmailPassword = (e) =>{
+    const signInEmailPassword = (e) => {
         e.preventDefault();
         signInWithEmailAndPassword(email, password)
     }
     return (
-      <div className='login-container'>
+        <div className='login-container'>
             <div className='form-container'>
-            <div>
-                <h2 className='form-title'>Login</h2>
-                <form onSubmit={signInEmailPassword}>
-                    <div className="form-group">
-                        <label htmlFor="Email">Email</label>
-                        <input onBlur={handleEmail} type="email" name="email" id="" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input onBlur={handlePassword} type="password" name="password" id="" />
-                    </div>
-                    <p>{error?.message.slice(22, 42)}</p>
-                    {loading &&<p>Loading...</p>}
-                    <input className='submit-input' type="submit" value="Login" />
-                </form>
-                <p>
-                    new to ema-john?<Link to='/signup'>Create New Account</Link>
-                </p>
-                <div className='or-container'>
-                    <p className='or-left'></p>
-                    <p>or</p>
-                    <p className='or-right'></p>
-                </div>
                 <div>
-                    <button className='google-button'> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2R_7bMO9d6c7f-DPHuqUinDwpqNF00jKnrQ&usqp=CAU" alt="" /> Continue with Google</button>
+                    <h2 className='form-title'>Login</h2>
+                    <form onSubmit={signInEmailPassword}>
+                        <div className="form-group">
+                            <label htmlFor="Email">Email</label>
+                            <input onBlur={handleEmail} type="email" name="email" id="" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input onBlur={handlePassword} type="password" name="password" id="" />
+                        </div>
+                        <p>{error?.message.slice(22, 42)}</p>
+                        {loading && <p>Loading...</p>}
+                        <input className='submit-input' type="submit" value="Login" />
+                    </form>
+                    <p>
+                        new to ema-john?<Link to='/signup'>Create New Account</Link>
+                    </p>
+                    <div className='or-container'>
+                        <p className='or-left'></p>
+                        <p>or</p>
+                        <p className='or-right'></p>
+                    </div>
+                    <div>
+                        <button className='google-button'> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2R_7bMO9d6c7f-DPHuqUinDwpqNF00jKnrQ&usqp=CAU" alt="" /> Continue with Google</button>
+                    </div>
                 </div>
             </div>
         </div>
-      </div>
     );
 };
 
